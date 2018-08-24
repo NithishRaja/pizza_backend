054d65b84a0f3849b87c0c72df6e469f1636294f
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-24 13:23:52 +0530

    finish delete logic for users route

--- router/users/delete.js
+++ router/users/delete.js
@@ -4,10 +4,31 @@
  */
 
 // Dependencies
+const _data = require("./../../lib/data");
 
 // delete function
 const remove = function(data, callback){
-  callback(200, {"message": "inside delete method"});
+  const userEmail = typeof(data.query.email)=="string"&&data.query.email.trim().length>0?data.query.email.trim():false;
+  // validating query parameter
+  if(userEmail){
+    // Getting user data
+    _data.read(userEmail, "users", function(err, userData){
+      if(!err&&userData){
+        // Deleting user
+        _data.delete(userEmail, "users", function(err){
+          if(!err){
+            callback(200);
+          }else{
+            callback(500, {"Error": "Unable to delete user file"});
+          }
+        });
+      }else{
+        callback(403, {"Error": "User does not exist"});
+      }
+    });
+  }else{
+    callback(400, {"Error": "Required fields missing"});
+  }
 };
 
 // Exporting module
