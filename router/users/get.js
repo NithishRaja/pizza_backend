9d3b979622d3b80b8520d98d27f24cff013b33b5
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-24 13:07:48 +0530

    finish get logic for user route

--- router/users/get.js
+++ router/users/get.js
@@ -4,10 +4,26 @@
  */
 
 // Dependencies
+const _data = require("./../../lib/data");
+const _helpers = require("./../../lib/helpers");
 
 // Get function
 const get = function(data, callback){
-  callback(200, {"message": "inside get method"});
+  const userEmail = typeof(data.query.email)=="string"&&data.query.email.trim().length>0?data.query.email.trim():false;
+  // Checking query parameter validity
+  if(userEmail){
+    // Reading from file
+    _data.read(userEmail, "users", function(err, userData){
+      if(!err&&userData){
+        const userDataObject = _helpers.parse(userData);
+        callback(200, userDataObject);
+      }else{
+        callback(403, {"Error": "User doen not exist"});
+      }
+    });
+  }else{
+    callback(400, {"Error": "Required fiels missing"});
+  }
 };
 
 // Exporting module
