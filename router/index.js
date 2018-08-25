f7800a5db9553e084c40054db6dacfa84d8ba6ad
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-08-25 15:23:53 +0530

    add logic to get method of menu route

--- router/index.js
+++ router/index.js
@@ -8,12 +8,14 @@ const ping = require("./ping");
 const notFound = require("./notFound");
 const users = require("./users");
 const tokens = require("./tokens");
+const menu = require("./menu");
 
 // Container object
 const router = {
   'ping': ping,
   'users': users,
   'tokens': tokens,
+  'menu': menu,
   'notFound': notFound
 };
 
