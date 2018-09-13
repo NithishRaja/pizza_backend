115d6cf48fc62fc668b0658a2da6f8554f4c067d
NithishRaja <f2016388@hyderabad.bits-pilaani.ac.in>
2018-09-13 10:49:10 +0530

    add functionality to create charges to stripe

--- router/order/post.js
+++ router/order/post.js
@@ -10,15 +10,10 @@ const util = require("util");
 const debug = util.debuglog("order");
 const config = require("./../../config");
 const _data = require("./../../lib/data");
+const _cart = require("./../../lib/cart");
 
 // Post function
 // NOTE: Payment via stripe is a two step process. Both client side and server side are involved. Here client sends a token which is used to send the API request
-// TODO: Add actual items to menu
-// TODO: Get items in cart and calculate total
-// TODO: Send request to stripe
-// TODO: Empty cart
-// TODO: Send mail to user via mailgun API
-// TODO: Remove todos oncee done
 const post = function(data, callback){
   const tokenId = typeof(data.headers.token)=="string"&&data.headers.token.trim().length==20?data.headers.token.trim():false;
   const amount = typeof(data.payload.amount)=="number"?data.payload.amount:false;
@@ -30,6 +25,7 @@ const post = function(data, callback){
     // Getting token data and checking if token has expired
     _data.read(tokenId, "tokens", function(err, tokenData){
       if(!err&&tokenData&&tokenData.timeOfExpiry>Date.now()){
+        // Sending request to stripe
         // Parsing url
         const parsedUrl = url.parse(config.stripeUrl);
         // Setting request details
@@ -40,23 +36,21 @@ const post = function(data, callback){
           'path': parsedUrl.path,
           'timeout': 5*1000
         };
-        callback(200, {"Message":"OK"});
-        /*
         // Initializing request
         const req = https.request(requestDetails, function(res){
-          const responseCode = res.statusCode;
-          if(res.body){
-            console.log(res.body);
+          if(res.statusCode==200){
+            callback(res.statusCode, {"Message": "Payment successful"});
+            // TODO: Empty cart
+            // TODO: Send mail to user via mailgun API
+          }else if(res.statusCode>=400){
+            callback(res.statusCode, {"Message": "Payment failed"});
           }
-          console.log("Response code: ", responseCode);
-          callback(200);
         });
         // Setting headers
         req.setHeader("Content-Type", "application/x-www-form-urlencoded");
         req.setHeader("Authorization", "Bearer "+config.stripeAPIKey);
         // Preparing payload
-        const payloadString="amount="+amount+"&&currency="+currency+"&&source="+source+"&&destination[account]=acct_1D7cGjHit3W4w74z";
-        console.log(payloadString);
+        const payloadString="amount="+amount+"&&currency="+currency+"&&source="+source;
         // Listening to error
         req.on("error", function(err){
           debug("Error while sending request to stripe", err);
@@ -65,14 +59,13 @@ const post = function(data, callback){
         // Listening to timeout
         req.on("timeout", function(err){
           debug("Request to stripe timedout", err);
-          callback(500, {"Error": "Falied to complete transaction"});
+          callback(500, {"Error": "Transaction request timed out"});
         });
         // Settind payload
         req.write(payloadString, function(){
           // Sending request
           req.end();
         });
-        */
       }else{
         callback(403, {"Error": "Token has expired or token does not exist"});
       }
